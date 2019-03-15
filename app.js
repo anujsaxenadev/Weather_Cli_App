1333c097149a37596d31ef8f8a1ba8fac16d37e4
Anuj <beaver@Beaver>
2019-03-15 16:41:37 +0530

    controlling the app

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// Main File
+const weather = require('./lib/weather');
+const print = require('./lib/print');
+const api = require('./api.json');
+const input = process.argv.slice(2);
+let url = "";
+
+if(input.length == 1){
+    url = `https://api.openweathermap.org/data/2.5/weather?zip=${input[0]}&units=metric&appid=${api.key}`;
+}
+else if(input.length == 2){
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${input[0]},${input[1]}&units=metric&appid=${api.key}`;
+}
+else{
+    const message = "Error in given inputs";
+    const statusCodeError = new Error(message);
+    print.error(statusCodeError);
+}
+
+weather.get(url);
